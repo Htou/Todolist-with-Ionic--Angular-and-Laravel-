61b69f67eae3f79479b369c362bc87b22cdfac79
Hichem Touati <hichem.touati@student.hu.nl>
2021-12-07 03:47:40 +0100

    added imports

--- Projects/mobile/ionic-angular/src/app/home/home.page.ts
+++ Projects/mobile/ionic-angular/src/app/home/home.page.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
-import { DataService, Message } from '../services/data.service';
+import {Component, OnInit} from '@angular/core';
+import {DataService, Message} from '../services/data.service';
+import {Todo, TodoService} from '../services/todo.service';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
-  constructor(private data: DataService) {}
+export class HomePage implements OnInit {
+  todos$: Todo[] = [];
+  validateForm!: FormGroup;
+
+  constructor(private fb: FormBuilder, private todoService: TodoService, private data: DataService) {
+    sumbitForm(value)
+  }
+
+  ngOnInit() {
+    this.validateForm = this.fb.group({
+      title: [null, Validators.required]
+    });
+  }
 
   refresh(ev) {
     setTimeout(() => {

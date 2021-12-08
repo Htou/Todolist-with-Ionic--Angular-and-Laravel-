0c8140801e2a9c49dce14c46514ee03f51d86e44
Hichem Touati <hichem.touati@student.hu.nl>
2021-12-08 23:19:15 +0100

    added todo post method

--- Projects/mobile/ionic-angular/src/app/home/home.page.ts
+++ Projects/mobile/ionic-angular/src/app/home/home.page.ts
@@ -12,11 +12,12 @@ export class HomePage implements OnInit {
   todos$: Todo[] = [];
   validateForm!: FormGroup;
 
-  constructor(private fb: FormBuilder, private todoService: TodoService, private data: DataService) {};
+  constructor(private fb: FormBuilder, private todoService: TodoService, private data: DataService) {
+  };
 
-  submitForm(value: { title: string; completed: false}): void {
-    for(const key in this.validateForm.controls) {
-      if(this.validateForm.controls.hasOwnProperty(key)){
+  submitForm(value: { title: string; completed: false }): void {
+    for (const key in this.validateForm.controls) {
+      if (this.validateForm.controls.hasOwnProperty(key)) {
         this.validateForm.controls[key].markAsDirty();
         this.validateForm.controls[key].updateValueAndValidity();
       }
@@ -39,6 +40,17 @@ export class HomePage implements OnInit {
       title: [null, Validators.required]
     });
   }
+
+  update = (todo) => {
+    const updateTodo = Object.assign({}, todo);
+    updateTodo.completed = !updateTodo.completed;
+    console.log('Todo updated', updateTodo);
+    this.todoService.update(updateTodo).then(() => {
+      this.todoService.findAll().then((res) => {
+        this.todos$ = res.data;
+      });
+    });
+  };
 
   refresh(ev) {
     this.todoService.findAll().then((res) => {
